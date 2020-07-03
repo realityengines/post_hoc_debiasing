4026a2213be03c78a571ecf04cb1dfbc07f6b56c
crwhite14 <colin@realityengines.ai>
2020-07-02 18:58:17 -0700

    printing from dataloader in notebook

--- celeb_race.py
+++ celeb_race.py
@@ -6,7 +6,6 @@ import numpy as np
 from torchvision.datasets import CelebA
 from torch.utils.data import Subset
 
-thresh = .7
 white = np.load(os.path.expanduser('~/post_hoc_debiasing/celebrace/white_full.npy'))
 black = np.load(os.path.expanduser('~/post_hoc_debiasing/celebrace/black_full.npy'))
 asian = np.load(os.path.expanduser('~/post_hoc_debiasing/celebrace/asian_full.npy'))
@@ -14,20 +13,16 @@ asian = np.load(os.path.expanduser('~/post_hoc_debiasing/celebrace/asian_full.np
 
 class CelebRace(CelebA):
 
-    #    def __init__(self, transform, split, root='./data', download=True):
-    #        super().__init__(transform=transform, split=split, root=root, download=download)
-
     def __getitem__(self, index):
 
         X, target = super().__getitem__(index)
 
-        race = [white[index] > .501, black[index] > .501, asian[index] > .501]
-        new = torch.tensor(race, dtype=torch.long)
+        augment = torch.tensor([white[index] > .501, black[index] > .501, asian[index] > .501, index+1], dtype=torch.long)
 
-        return X, torch.cat((target, new))
+        return X, torch.cat((target, augment))
 
 
-def unambiguous(dataset, split='train'):
+def unambiguous(dataset, split='train', thresh=.7):
 
     if split == 'train':
         n = 162770
@@ -38,13 +33,12 @@ def unambiguous(dataset, split='train'):
     return Subset(dataset, unambiguous_indices)
 
 
-def split_check(dataset, split='train'):
+def split_check(dataset, split='train', thresh=.7):
 
     if split == 'train':
         n = 162770
     else:
         n = 19962
-
     unambiguous_indices = [i for i in range(n) if (asian[i] > thresh)]
 
     return Subset(dataset, unambiguous_indices)
